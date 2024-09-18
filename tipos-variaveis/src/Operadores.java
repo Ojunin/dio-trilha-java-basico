f4d04a873dc3cfcc31fbc874573675cb148aa94d
DevJunin <waxjunior@hotmail.com>
2024-09-18 17:17:33 -0300

    Modulo Operadores Acrescentado

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,116 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /*String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+
+        ----------------------//---------------------//-------------------------
+
+        int numero = 5;
+        System.out.println(- numero);
+
+        System.out.println(numero);*/
+
+        /*int numero = 5;
+        //repetição
+        //numero ++;
+
+        System.out.println(++ numero);
+        System.out.println(-- numero);
+
+        ----------------------//---------------------//-------------------------
+
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+
+        System.out.println(variavel);
+
+        ----------------------//---------------------//-------------------------*/
+
+        /*OPERADOR TERNÁRIO ?
+
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ?"Verdadeiro" : "Falso";
+
+        EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        
+        if (a==b)
+            resultado = "Verdadeiro";
+        else
+            resultado = "Falso";      
+
+        System.out.println(resultado);
+
+        ----------------------//---------------------//-------------------------*/
+
+        /*OPERADOR RELACIONAL
+
+        String nomeUm = "JUNIOR";
+        String nomeDois = new String("JUNIOR");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2){
+            System.out.println("a nossa condição é verdadeira");
+        }
+        if(numero1 < numero2)
+            System.out.println("Número 1 menor que número 2");
+
+
+        System.out.println("numeroUm é igual a numeroDois?" + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente do numeroDois?" + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois?" + simNao);
+
+        simNao = numero1 < numero2;
+        System.out.println("numeroUm é menor que numeroDois" + simNao);
+
+        ----------------------//---------------------//-------------------------*/
+        
+        /*OPERADORES LÓGICOS*/
+
+        Boolean condicao1=true;
+
+        Boolean condicao2=false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("As duas condiçoes são verdadeiras.");           
+        }
+        System.out.println("Fim");
+
+    }
+
+}
