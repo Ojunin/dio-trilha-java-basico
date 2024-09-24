2acfddb14740528ea07ccf8b167c5972dc2b4b41
junindev <waxjunior@hotmail.com>
2024-09-23 22:34:48 -0300

    Condição Simples, Composta, Expressão Ternária e Switch Case

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,11 +1,33 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 4;
+        
 
-        if(nota >= 7)
+        /*Condição Composta */
+
+        /*if(nota >= 7)
             System.out.println("Aprovado");
 
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Prova de Recuperação");        
+
         else
-            System.out.println("Reprovado");
+            System.out.println("Reprovado");*/
+
+
+
+            /*Expressão Ternária - Cenário1
+
+            String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+            System.out.println(resultado);
+
+
+
+            Expressão Ternária - Canário2*/
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >=5 && nota < 7 ? "Recuperação" : "Reprovado";
+
+        System.out.println(resultado);
     }
 }
